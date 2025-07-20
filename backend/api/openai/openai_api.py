91fd9040c2ee7e7b234f32a6927171b0be055177
Jose <joseangel130414@gmail.com>
2025-07-20 17:11:25 -0500

    feature/youtube-api: managed to schedule a stream with title and description generated by open api

--- backend/api/openai/openai_api.py
+++ backend/api/openai/openai_api.py
@@ -19,7 +19,7 @@ def gen_desc(prompt):
             {"role": "user", "content": f"Create me a title following this description for a youtube stream: {prompt}"},
             {"role": "user", "content": "Give me a description for the stream based on the title given by the previous prompt"},
             {"role": "user", "content": "Based on the description and on the title give me a prompt that i can give dall-3-e for a thubnail. Please make it to YouTube thubnail standards."},
-            {"role": "user", "content": "Make sure all of the responses start with the following format Response: and dont add quotations at the start and end of the response"}
+            {"role": "user", "content": "Make sure all of the responses start with the following format Title: and dont add quotations at the start and end of the response"}
         ],
         max_tokens=1000
     )
@@ -31,7 +31,7 @@ def gen_desc(prompt):
     matches = re.findall(r":\s*(.*)", api_response)
 
     for match in matches:
-        responses_text.append(match)
+        responses_text.append(match.strip())
 
 
     # call gen_image to generate the image based on the description
